122aa9f99abd7fd8e3e1c45a26236259727d85a2
ThaSMorato <tha.s.morato@gmail.com>
2022-04-19 14:26:14 -0300

    feat: processor facade

--- src/textProcessorFacade.js
+++ src/textProcessorFacade.js
@@ -1,1 +1,19 @@
 // TODO: Dê uma olhada no projeto oficial do módulo 06 (Expressões Regulares - RegExp) para implementar este arquivo.
+
+import { TextProcessorFluentApi } from "./textProcessorFluentAPI.js";
+
+export class TextProcessorFacade {
+  #textProcessorFluentAPI;
+  constructor({ TextProcessorApi }) {
+    this.#textProcessorFluentAPI = TextProcessorApi;
+  }
+
+  getProjectsFromCSV() {
+    return this.#textProcessorFluentAPI
+      .extractHeaders()
+      .splitContentKeys()
+      .createRawObject()
+      .mapProjects()
+      .build();
+  }
+}
