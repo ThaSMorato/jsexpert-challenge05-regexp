5e27213ab3bb5bf570e97f6867fc3ee794bcb85b
ThaSMorato <tha.s.morato@gmail.com>
2022-04-19 14:10:52 -0300

    feat: text fluent api

--- src/textProcessorFluentAPI.js
+++ src/textProcessorFluentAPI.js
@@ -1,1 +1,71 @@
 // TODO: Dê uma olhada no projeto oficial do módulo 06 (Expressões Regulares - RegExp) para implementar este arquivo.
+
+import { evaluateRegex } from "./util.js";
+import { Project } from "./project.js";
+
+export class TextProcessorFluentApi {
+  #content;
+
+  constructor(content) {
+    this.#content = { content };
+  }
+
+  extractHeaders() {
+    const matchHeaders = evaluateRegex(/(título)(.*)\n/gim);
+    const headers = this.#content.content.match(matchHeaders)[0];
+
+    this.#content = {
+      headers,
+      content: this.#content.content.replace(matchHeaders, ""),
+    };
+
+    return this;
+  }
+
+  splitContentKeys() {
+    const splitKey = evaluateRegex(/;/gim);
+    const splitLineKey = evaluateRegex(/\n/gim);
+
+    const splitedHeaders = this.#content.headers
+      .split(splitKey)
+      .filter((item) => !item.match(splitLineKey));
+
+    const splitedContent = this.#content.content
+      .split(splitLineKey)
+      .map((line) => line.split(splitKey).filter((item) => item.length > 0));
+
+    this.#content = {
+      headers: splitedHeaders,
+      content: splitedContent,
+    };
+
+    return this;
+  }
+
+  createRawObject() {
+    const mapFunction = (itemArray) => {
+      return itemArray.reduce((prev, current, currentIndex) => {
+        return {
+          ...prev,
+          [this.#content.headers[currentIndex]]: current,
+        };
+      }, {});
+    };
+
+    this.#content = {
+      content: this.#content.content.map(mapFunction),
+    };
+
+    return this;
+  }
+
+  mapProjects() {
+    this.#content = this.#content.content.map((content) => new Project(content));
+
+    return this;
+  }
+
+  build() {
+    return this.#content;
+  }
+}
