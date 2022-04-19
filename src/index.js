c94cf02ee221e32e2a9f59e9da96b2b1f52577ae
ThaSMorato <tha.s.morato@gmail.com>
2022-04-19 15:15:44 -0300

    feat: index js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
 // TODO: Dê uma olhada no projeto oficial do módulo 06 (Expressões Regulares - RegExp) para implementar este arquivo.
 // Dica do wells: no lugar da chamada do PDF parser, um simples `.toString()` resolve, já que nós é que implementaremos o "CSV parser"
+
+import { readFile } from "fs/promises";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+
+import { TextProcessorFacade } from "./textProcessorFacade.js";
+import { TextProcessorFluentApi } from "./textProcessorFluentAPI.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const csvDir = join(__dirname, "../docs/projeto-de-lei.csv");
+
+const dataBuffer = await readFile(csvDir);
+const data = dataBuffer.toString();
+
+const api = new TextProcessorFluentApi(data);
+const instance = new TextProcessorFacade({ TextProcessorApi: api });
+const projects = instance.getProjectsFromCSV();
+
+console.log(projects);
